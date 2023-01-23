bd24ccf457819c730053f8d1f00582409afb14ef
Robertspoon <robertthemcjug@gmail.com>
2023-01-23 15:13:17 -0400

    Initial commit

---
+++ TextBasedRPG/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBasedRPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Text based RPG");
+
+            Console.ReadKey(true);
+        }
+    }
+}
