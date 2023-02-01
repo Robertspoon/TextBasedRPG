a0dbcac6b0f0f0206c6b1dd5a3b6417ed0aa88b9
Robertspoon <robertthemcjug@gmail.com>
2023-01-31 21:00:33 -0400

    added proto-main menu

--- TextBasedRPG/Program.cs
+++ TextBasedRPG/Program.cs
@@ -11,13 +11,15 @@ namespace TextBasedRPG
         
         static void Main(string[] args)
         {
-            //Console.WriteLine("Text based RPG");
-           // Console.WriteLine();
-           // Console.WriteLine("Press any key to start.....");
+            Console.WriteLine("Text based RPG");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to start.....");
+            Console.ReadKey(true);
+            Console.Clear();
 
             Console.CursorVisible = false;
 
-            //GameCharacter gameCharacter = new GameCharacter();
+            
             Map map = new Map();
             Player player = new Player("Jerald",100,map,'P');
             Enemy enemy = new Enemy("Rat",25,map,'E');
@@ -28,7 +30,7 @@ namespace TextBasedRPG
                player.Draw();
                enemy.Draw();
                player.Update();
-               enemy.Update();
+               //enemy.Update();
             }
 
             Console.ReadKey(true);
